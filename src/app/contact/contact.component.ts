cf4a2f794bc2c8fd825550869a55c69118b3a774
sieu-nguyen <sieu.nguyen@codeleap.de>
2025-02-19 15:57:20 +0700

    create contact component

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  imports: [],
+  template: ` <p class="text-black dark:text-white">contact works!</p> `,
+  styles: ``,
+})
+export class ContactComponent {}
