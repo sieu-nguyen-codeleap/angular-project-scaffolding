5b882037b15e5b39fd64c85bee42927acb3e490f
sieu-nguyen <sieu.nguyen@codeleap.de>
2025-02-19 15:58:50 +0700

    create team component

---
+++ src/app/team/team.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team',
+  imports: [],
+  template: ` <p class="text-black dark:text-white">team works!</p> `,
+  styles: ``,
+})
+export class TeamComponent {}
