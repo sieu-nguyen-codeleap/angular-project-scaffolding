e32c72bb63701be3d366c11b1cfca1a707512863
sieu-nguyen <sieu.nguyen@codeleap.de>
2025-02-19 15:56:23 +0700

    create company component

---
+++ src/app/company/company.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company',
+  imports: [],
+  template: ` <p>company works!</p> `,
+  styles: ``,
+})
+export class CompanyComponent {}
