14e4c4df324c77b970b42a898932837479ddb5ec
sieu-nguyen <sieu.nguyen@codeleap.de>
2025-02-19 15:58:39 +0700

    create market place component

---
+++ src/app/market-place/market-place.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-market-place',
+  imports: [],
+  template: ` <p class="text-black dark:text-white">market-place works!</p> `,
+  styles: ``,
+})
+export class MarketPlaceComponent {}
