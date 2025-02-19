9d541514d6690fbf3e124be16c1f6c8e4feb19fb
sieu-nguyen <sieu.nguyen@codeleap.de>
2025-02-19 15:57:54 +0700

    create features component

---
+++ src/app/features/features.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-features',
+  imports: [],
+  template: ` <p class="text-black dark:text-white">features works!</p> `,
+  styles: ``,
+})
+export class FeaturesComponent {}
