b07737250b234225433df8b2371c8cfecf8db279
sieu-nguyen <sieu.nguyen@codeleap.de>
2025-02-19 15:56:02 +0700

    feat : route path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'company',
+    loadComponent: () =>
+      import('./company/company.component').then((m) => m.CompanyComponent),
+  },
+  {
+    path: 'market-place',
+    loadComponent: () =>
+      import('./market-place/market-place.component').then(
+        (m) => m.MarketPlaceComponent,
+      ),
+  },
+  {
+    path: 'features',
+    loadComponent: () =>
+      import('./features/features.component').then((m) => m.FeaturesComponent),
+  },
+  {
+    path: 'team',
+    loadComponent: () =>
+      import('./team/team.component').then((m) => m.TeamComponent),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./contact/contact.component').then((m) => m.ContactComponent),
+  },
+];
